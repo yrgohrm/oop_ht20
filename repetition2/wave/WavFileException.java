512ea3e1f4d99301d3bcf0519c47196c2e48cd5b
Hampus Ram <hampus.ram@educ.goteborg.se>
2020-10-01 08:18:15 +0200

    Wavfile library to use in examples and excercises.

---
+++ repetition2/wave/WavFileException.java
@@ -0,0 +1,26 @@
+package wave;
+
+public class WavFileException extends Exception
+{
+	private static final long serialVersionUID = 5863351222418063283L;
+
+	public WavFileException()
+	{
+		super();
+	}
+
+	public WavFileException(String message)
+	{
+		super(message);
+	}
+
+	public WavFileException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public WavFileException(Throwable cause) 
+	{
+		super(cause);
+	}
+}
---
+++ repetition2/wave/WriteExample.java
@@ -0,0 +1,56 @@
+package wave;
+
+import java.io.*;
+
+public class WriteExample
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			int sampleRate = 44100;		// Samples per second
+			double duration = 5.0;		// Seconds
+
+			// Calculate the number of frames required for specified duration
+			long numFrames = (long)(duration * sampleRate);
+
+			// Create a wav file with the name specified as the first argument
+			WavFile wavFile = WavFile.newWavFile(new File(args[0]), 2, numFrames, 16, sampleRate);
+
+			// Create a buffer of 100 frames
+			double[][] buffer = new double[2][100];
+
+			// Initialise a local frame counter
+			long frameCounter = 0;
+
+			// Loop until all frames written
+			while (frameCounter < numFrames)
+			{
+				// Determine how many frames to write, up to a maximum of the buffer size
+				long remaining = wavFile.getFramesRemaining();
+				int toWrite = (remaining > 100) ? 100 : (int) remaining;
+
+				// C4 = 262, D = 294, E = 329, F = 349, G = 392, A = 440, B = 494, C5 = 523
+				double freq1 = 440;
+				double freq2 = 494;
+
+				// Fill the buffer, one tone per channel
+				for (int s=0 ; s<toWrite ; s++, frameCounter++)
+				{
+					buffer[0][s] = Math.sin(2.0 * Math.PI * freq1 * frameCounter / sampleRate);
+					buffer[1][s] = Math.sin(2.0 * Math.PI * freq2 * frameCounter / sampleRate);
+				}
+
+				// Write the buffer
+				wavFile.writeFrames(buffer, toWrite);
+			}
+
+			// Close the wavFile
+			wavFile.close();
+		}
+		catch (Exception e)
+		{
+			System.err.println(e);
+		}
+	}
+}
