a769c5611aea8671764199ce6190d5a2efd53efc
Hampus Ram <hampus.ram@educ.goteborg.se>
2020-10-22 14:47:29 +0200

    minor update

--- lektion16_real/App.java
+++ lektion16_real/App.java
@@ -1,9 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        int[] apa;
-        double bepa;
-
-        System.out.println(int[].class.);
+        System.out.println(int[].class.getName());
         System.out.println(double.class.getName());
     }
 }
